75e2de2f01c77b8f3361d30ebc012b15d93642cb
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-02-09 18:06:49 +0100

    allocation of a common piece of code into a separate component MatchTable.jsx to remove code duplication

--- src/components/MatchList.jsx
+++ src/components/MatchList.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import {makeStyles} from "@material-ui/core/styles";
-import {Box, Paper, Table, TableBody, TableContainer, TableHead, Typography, withStyles} from "@material-ui/core";
+import {Typography} from "@material-ui/core";
 import {useQuery} from "@apollo/react-hooks";
 import ErrorIcon from "@material-ui/icons/Error";
 
 import {MATCHES_IN_PROGRESS, PLAYED_MATCHES} from "../graphql/queries";
-import TableRow from "@material-ui/core/TableRow";
-import TableCell from "@material-ui/core/TableCell";
+import MatchTable from "./MatchTable";
 
 const useStyles = makeStyles((_) => ({
     root: {
@@ -23,11 +22,6 @@ const useStyles = makeStyles((_) => ({
 
 function MatchList() {
     const classes = useStyles();
-    // const {
-    //     loading,
-    //     error,
-    //     data
-    // } = useQuery(GET_ALL_MATCHES);
 
     const platedMatchesQuery = useQuery(PLAYED_MATCHES);
     const playedMatchesLoading = platedMatchesQuery.loading;
@@ -38,24 +32,6 @@ function MatchList() {
     const matchesInProgressLoading = matchesInProgressQuery.loading;
     const matchesInProgressError = matchesInProgressQuery.error;
     const matchesInProgressData = matchesInProgressQuery.data;
-
-    const StyledTableCell = withStyles((theme) => ({
-        head: {
-            backgroundColor: theme.palette.common.black,
-            color: theme.palette.common.white,
-        },
-        body: {
-            fontSize: 14,
-        },
-    }))(TableCell);
-
-    const StyledTableRow = withStyles((theme) => ({
-        root: {
-            '&:nth-of-type(odd)': {
-                backgroundColor: theme.palette.action.hover,
-            },
-        },
-    }))(TableRow);
 
     if (playedMatchesLoading || matchesInProgressLoading) return "Loading...";
     if (playedMatchesError)
@@ -78,90 +54,12 @@ function MatchList() {
         <>
             <Container className={classes.root}>
                 <Typography variant="h2">MATCHES IN PROGRESS</Typography>
-                <Box>
-                    {matchesInProgressData.matches.map((match) => (
-                        <article key={match.id} className={classes.article}>
-                            <p>Match ID: {match.id}</p>
-                            <p>Match date: {match.started_at}</p>
-
-                            <TableContainer component={Paper}>
-                                <Table className={classes.table} aria-label="customized table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <StyledTableCell align="left">Players</StyledTableCell>
-                                            {match.setts?.map((sett, index) => {
-                                                return <StyledTableCell>set {index + 1}</StyledTableCell>
-                                            })}
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        <StyledTableRow>
-                                            <StyledTableCell>{match.p1.name}</StyledTableCell>
-                                            {match.setts?.map((sett, index) => {
-                                                return <StyledTableCell key={index}>{sett.p1_score}</StyledTableCell>
-                                            })}
-                                        </StyledTableRow>
-                                        <StyledTableRow>
-                                            <StyledTableCell>{match.p2.name}</StyledTableCell>
-                                            {match.setts?.map((sett, index) => {
-                                                return <StyledTableCell key={index}>{sett.p1_score}</StyledTableCell>
-                                            })}
-                                        </StyledTableRow>
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                            {match.winner === null
-                             ? <p>Winner: match is in process</p>
-                             : <p>Winner: {match.winner?.name}</p>
-                            }
-                            <hr/>
-                        </article>
-                    ))}
-                </Box>
+                <MatchTable data={matchesInProgressData}/>
             </Container>
 
             <Container className={classes.root}>
                 <Typography variant="h2">PLAYED MATCHES</Typography>
-                <Box>
-                    {playedMatchesData.matches.map((match) => (
-                        <article key={match.id} className={classes.article}>
-                            <p>Match ID: {match.id}</p>
-                            <p>Match date: {match.started_at}</p>
-
-                            <TableContainer component={Paper}>
-                                <Table className={classes.table} aria-label="customized table">
-                                    <TableHead>
-                                        <TableRow>
-                                            <StyledTableCell align="left">Players</StyledTableCell>
-                                            {match.setts?.map((sett, index) => {
-                                                return <StyledTableCell>set {index + 1}</StyledTableCell>
-                                            })}
-                                        </TableRow>
-                                    </TableHead>
-                                    <TableBody>
-                                        <StyledTableRow>
-                                            <StyledTableCell>{match.p1.name}</StyledTableCell>
-                                            {match.setts?.map((sett, index) => {
-                                                return <StyledTableCell key={index}>{sett.p1_score}</StyledTableCell>
-                                            })}
-                                        </StyledTableRow>
-                                        <StyledTableRow>
-                                            <StyledTableCell>{match.p2.name}</StyledTableCell>
-                                            {match.setts?.map((sett, index) => {
-                                                return <StyledTableCell key={index}>{sett.p1_score}</StyledTableCell>
-                                            })}
-                                        </StyledTableRow>
-                                    </TableBody>
-                                </Table>
-                            </TableContainer>
-                            {match.winner === null
-                             ? <p>Winner: match is in process</p>
-                             : <p>Winner: {match.winner?.name}</p>
-                            }
-                            <hr/>
-                        </article>
-                    ))}
-                </Box>
+                <MatchTable data={playedMatchesData}/>
             </Container>
         </>
     );
--- src/components/MatchTable.jsx
+++ src/components/MatchTable.jsx
@@ -1,5 +1,85 @@
-function MatchTable() {
+import {Box, Paper, Table, TableBody, TableContainer, TableHead, withStyles} from "@material-ui/core";
+import TableRow from "@material-ui/core/TableRow";
+import React from "react";
+import TableCell from "@material-ui/core/TableCell";
+import {makeStyles} from "@material-ui/core/styles";
 
+const useStyles = makeStyles((_) => ({
+    root: {
+        padding: "2em",
+    },
+    table: {
+        minWidth: 200,
+    },
+    article: {
+        marginBottom: "60px",
+    }
+}));
+
+function MatchTable(props) {
+    const classes = useStyles();
+    const data = props.data;
+
+    const StyledTableCell = withStyles((theme) => ({
+        head: {
+            backgroundColor: theme.palette.common.black,
+            color: theme.palette.common.white,
+        },
+        body: {
+            fontSize: 14,
+        },
+    }))(TableCell);
+
+    const StyledTableRow = withStyles((theme) => ({
+        root: {
+            '&:nth-of-type(odd)': {
+                backgroundColor: theme.palette.action.hover,
+            },
+        },
+    }))(TableRow);
+
+    return(
+        <Box>
+            {data.matches.map((match) => (
+                <article key={match.id} className={classes.article}>
+                    <p>Match ID: {match.id}</p>
+                    <p>Match date: {match.started_at}</p>
+
+                    <TableContainer component={Paper}>
+                        <Table className={classes.table} aria-label="customized table">
+                            <TableHead>
+                                <TableRow>
+                                    <StyledTableCell align="left">Players</StyledTableCell>
+                                    {match.setts?.map((sett, index) => {
+                                        return <StyledTableCell>set {index + 1}</StyledTableCell>
+                                    })}
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <StyledTableRow>
+                                    <StyledTableCell>{match.p1.name}</StyledTableCell>
+                                    {match.setts?.map((sett, index) => {
+                                        return <StyledTableCell key={index}>{sett.p1_score}</StyledTableCell>
+                                    })}
+                                </StyledTableRow>
+                                <StyledTableRow>
+                                    <StyledTableCell>{match.p2.name}</StyledTableCell>
+                                    {match.setts?.map((sett, index) => {
+                                        return <StyledTableCell key={index}>{sett.p1_score}</StyledTableCell>
+                                    })}
+                                </StyledTableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                    {match.winner === null
+                     ? <p>Winner: match is in process</p>
+                     : <p>Winner: {match.winner?.name}</p>
+                    }
+                    <hr/>
+                </article>
+            ))}
+        </Box>
+    )
 }
 
 
